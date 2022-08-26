58eac7704a78b6246ec60ece94fdc41c12802bad
Oscar Mulder <oscar.mulder@codam.nl>
2022-08-26 15:15:28 +0200

    feat: add release automation

---
+++ release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    branches: ["main"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/npm", {
+            "tarballDir": "dist",
+        }],
+        ["@semantic-release/github", {
+            "assets": "dist/*.tgz"
+        }]
+    ]
+};
