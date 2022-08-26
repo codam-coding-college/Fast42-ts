13de641fe7f4bf28910374fc35d54cb614a1063f
Oscar Mulder <oscar.mulder@codam.nl>
2022-08-26 18:52:20 +0200

    fix: cleanup package.json

--- release.config.js
+++ release.config.js
@@ -3,11 +3,7 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        ["@semantic-release/npm", {
-            "tarballDir": "dist",
-        }],
-        ["@semantic-release/github", {
-            "assets": "dist/*.tgz"
-        }]
+        "@semantic-release/npm",
+        "@semantic-release/github",
     ]
 };
